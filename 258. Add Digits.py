1fb2e702a9ac7d6a42d7ae9c9518d036dad8f99c
WQGHG <752362058@qq.com>
2016-05-05 18:21:49 +0800

    Create 258. Add Digits.py

---
+++ 258. Add Digits.py
@@ -0,0 +1,13 @@
+class Solution:
+    # @param {integer} num
+    # @return {integer}
+    def addDigits(self, num):
+        length = len(str(num))
+        while num>=10:
+            L = []
+            for i in reversed(range(length)):
+                l = num/10**i
+                num = num%(10**i)
+                L.append(l)
+            num = sum(L)
+        return num
